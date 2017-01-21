790778cfd75f8d8cd9f7b78a5b15d58e34782f78
Marek Bielik <bielik@zulapost.net>
2017-01-21 11:12:02 +0100

    simulation output

---
+++ main.h
@@ -0,0 +1,19 @@
+//
+// Created by marek on 11/22/16.
+//
+
+#ifndef BT_MAIN_H
+#define BT_MAIN_H
+
+#include <sharedspice.h>
+#include <iostream>
+#include <stdio.h>
+
+ControlledExit ng_exit;
+SendData ng_data;
+SendInitData ng_initData;
+SendChar ng_getchar;
+SendStat ng_getstat;
+
+
+#endif //BT_MAIN_H
